d4d74b18bb7650bc49abfb29a56efbc21046d297
Camila Maia <camilamaia@poli.ufrj.br>
2021-05-03 15:09:33 -0300

    Mudança init.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask 
 
+from app.cadastro_alunos.model import Alunos
+from app.cadastro_professores.model import Professores
+
+
+
 def create_app():
     app = Flask(__name__)
-    
