871814f8366b375f3b6cc3559030feb7f66a57b2
Camila Maia <camilamaia@poli.ufrj.br>
2021-05-17 16:42:04 -0300

    deploy

--- app/config.py
+++ app/config.py
@@ -1,8 +1,10 @@
 from app.sensive import Sensive as sensive
+from os import environ
 
 class Config: 
-    SQLALCHEMY_DATABASE_URI = sensive.SQLALCHEMY_DATABASE_URI
-    SQLALCHEMY_TRACK_MODIFICATIONS = sensive.SQLALCHEMY_TRACK_MODIFICATIONS
-    JSON_SORT_KEYS = sensive.JSON_SORT_KEYS
+    SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URL = 'postgres://awmxbjemscipfr:00a94aee34e26ff35d55b9a8fe66baf572c3a73057cc3b06e21666ce38ea8be0@ec2-18-215-111-67.compute-1.amazonaws.com:5432/dek5p97di3qk5h'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JSON_SORT_KEYS = False
 
     #JWT_SECRET_KEY = sensive.JWT_SECRET_KEY
