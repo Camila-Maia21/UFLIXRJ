6913f1d52899ec22dcf95c23a59e4b1bde8f6149
Camila Maia <camilamaia@poli.ufrj.br>
2021-05-17 16:53:32 -0300

    deploy

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ from os import environ
 
 class Config: 
     SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URI')
-    SQLALCHEMY_DATABASE_URL = 'postgresql://oqnquahjgszyqj:483eee03100b1ab916889ecc904e65cfe8c61342ccfa021fd58e6be95d88fdc1@ec2-34-193-113-223.compute-1.amazonaws.com:5432/dcensg8a8knisj'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://oqnquahjgszyqj:483eee03100b1ab916889ecc904e65cfe8c61342ccfa021fd58e6be95d88fdc1@ec2-34-193-113-223.compute-1.amazonaws.com:5432/dcensg8a8knisj'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JSON_SORT_KEYS = False
 
