b1e8c39e0fa94065acaec2cde586db452a17faaa
Camila Maia <camilamaia@poli.ufrj.br>
2021-05-17 20:00:39 -0300

    deploy

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from app.config import Config
 from app.cadastro_alunos.routes import aluno_api
 from app.cadastro_professores.routes import professor_api
 from app.materia.routes import materia_api
+from app.login.routes import login_api
 
 
 def create_app():
@@ -17,6 +18,7 @@ def create_app():
     app.register_blueprint(professor_api)
     app.register_blueprint(aluno_api)
     app.register_blueprint(materia_api)
+    app.register_blueprint(login_api)
 
 
     return app
