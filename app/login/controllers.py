f365eca66440d1617e2973e638308d14cea4c142
Camila Maia <camilamaia@poli.ufrj.br>
2021-05-17 19:31:52 -0300

    deploy

--- app/login/controllers.py
+++ app/login/controllers.py
@@ -1,10 +1,31 @@
 from app.login.model import Login
+from app.cadastro_professores.model import Professor
+from app.cadastro_alunos.model import Aluno
 from app.extensions import db
 from flask import jsonify, request 
 from flask.views import MethodView
 import bcrypt 
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
+
+class UserLogin(MethodView):  #/login
+
+    def post(self): 
+
+        dados = Login().load(request.json)
+        aluno = Aluno.query.filter_by(email=dados['email']).first()
+        professor = Professor.query.filter_by(email=dados['email']).first()
+
+        if not aluno or not aluno.verify_password(dados['password']):
+            return {'error': 'email ou password incorretos'}, 401
+
+        if not professor or not professor.verify_password(dados['password']):
+            return {'error': 'email ou password incorretos'}, 401
+
+        token = create_access_token(identity=aluno.id) 
+
+        return {'token': token}, 200 
+'''
 class LoginDetails(MethodView): #login
     def get(self):
         login = Login.query.all()
@@ -46,3 +67,4 @@ class UsuarioLogin(MethodView): #usuariologin
         token = create_access_token(identity=login.id)
         
         return {"token": token}, 200
+'''
