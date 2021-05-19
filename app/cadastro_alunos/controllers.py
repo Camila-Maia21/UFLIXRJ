faaca3681113f7cf1a8d5910f3ee6eeb150d65f5
Camila Maia <camilamaia@poli.ufrj.br>
2021-05-19 10:55:19 -0300

    agora vai

--- app/cadastro_alunos/controllers.py
+++ app/cadastro_alunos/controllers.py
@@ -7,42 +7,7 @@ from flask import Blueprint
 from flask import render_template, abort
 from jinja2 import TemplateNotFound
 from flask import redirect
-'''
-app_aluno = Blueprint('simple_page', __name__, template_folder='templates')
 
-@app_aluno.route('/aluno', defaults={'cadastroAluno': 'index'})
-def criar_aluno():
-    if request.method == 'POST':
-
-        dados = request.json
-        nome = dados.get('nome')
-        email = dados.get('email')
-        cpf = dados.get('cpf')
-        dre = dados.get('dre')
-        curso = dados.get('curso')
-        senha = str(dados.get('senha'))
-
-        if not isinstance(nome, str) or not isinstance(email, str) or not isinstance(cpf, str) or not isinstance(dre, str) or not isinstance(curso, str):
-            return {"error" : "Algum tipo invalido"}, 400
-
-        senha_hash = bcrypt.hashpw(senha.encode(), bcrypt.gensalt()) #criptografa senha e adiciona um "sal"
-
-        aluno = Aluno(nome=nome, email=email , cpf=cpf, dre=dre, curso=curso, senha_hash=senha_hash)
-
-        db.session.add(aluno)
-        db.session.commit()
-
-    return aluno.json(), 200
-
-@app_aluno.route('/aluno')
-def rota_aluno(cadastroAluno):
-    try:
-        return render_template(f'template/cadastroAluno.html')
-    except TemplateNotFound:
-        abort(404)
-
-    
-'''
 class AlunoDetails(MethodView): #/aluno
 
     def get(self):
